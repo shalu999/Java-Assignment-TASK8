914e73e947f19fa998ede49071914592ddb7e19c
shalu999 <shaluaugusthy93@gmail.com>
2024-04-17 22:19:38 +0100

    new program

--- Assisgnment/src/JavaAssisment/Primenumber.java
+++ Assisgnment/src/JavaAssisment/Primenumber.java
@@ -21,7 +21,8 @@ public class Primenumber {
 		  
 		  {  
 		   for(int i=2;i<=m;i++){      
-		    if(n%i==0)
+		   
+			   if(n%i==0)
 		    {      
 		     System.out.println(n+" is not prime number");  
 		     
