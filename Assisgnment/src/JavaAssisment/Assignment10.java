bb60ecfd5e164e0fcee188b89f29bfff210f2b29
shalu999 <shaluaugusthy93@gmail.com>
2024-04-17 22:19:11 +0100

    Added prime number program

--- Assisgnment/src/JavaAssisment/Assignment10.java
+++ Assisgnment/src/JavaAssisment/Assignment10.java
@@ -1,9 +1,22 @@
 package JavaAssisment;
 
+import java.util.Scanner;
+
 public class Assignment10 {
 
 	public static void main(String[] args) {
-		
+		Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();	
+        int i,j=0;
+        
+        if (number<=1) {
+        	System.out.println(" The number is not prime");
+        }
+		for (i=2;i<=j;i++)
+		{
+			
+		}
 
 	}
 
